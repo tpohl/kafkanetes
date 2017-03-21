97bb0c409c104492482e78f64113d18cf18f9844
Thorsten Pohl <thorsten.pohl@lht.dlh.de>
2017-03-21 17:01:55 +0100

    MYID back in /tmp/zookeeper

--- zookeeper-server-start-multiple.sh
+++ zookeeper-server-start-multiple.sh
@@ -5,7 +5,7 @@ PREFIX=$2
 MYID=$3
 MAXID=$4
 
-echo $MYID >$JAVA_DATA_DIR/tmp/zookeeper/myid
+echo $MYID >/tmp/zookeeper/myid
 for ((i=1; i<=$MAXID; i++)); do
   echo server.$i=$PREFIX-$i:2888:3888
 done >>$PROPERTIES
