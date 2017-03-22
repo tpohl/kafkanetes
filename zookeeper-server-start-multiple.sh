7ade251d2e18a4ceee5bd24a20988229aed9b9a4
Thorsten Pohl <thorsten.pohl@lht.dlh.de>
2017-03-22 08:48:48 +0100

    zookeeper properties to data-deployments

--- zookeeper-server-start-multiple.sh
+++ zookeeper-server-start-multiple.sh
@@ -5,7 +5,7 @@ PREFIX=$2
 MYID=$3
 MAXID=$4
 
-echo $MYID >/tmp/zookeeper/myid
+echo $MYID >/data/deployments/tmp/zookeeper/myid
 for ((i=1; i<=$MAXID; i++)); do
   echo server.$i=$PREFIX-$i:2888:3888
 done >>$PROPERTIES
@@ -14,6 +14,11 @@ quorumListenOnAllIPs=true
 tickTime=2000
 initLimit=10
 syncLimit=5
+dataDir=/data/deployments/tmp/zookeeper
+# the port at which the clients will connect
+clientPort=2181
+# disable the per-ip limit on the number of connections since this is a non-production config
+maxClientCnxns=0
 EOF
 
 exec bin/zookeeper-server-start.sh $PROPERTIES
